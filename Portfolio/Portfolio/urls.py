8bb5773eed3c6e199e83b90bb2b1b719c757093e
xyclose11 <harrisgus1@hotmail.com>
2023-11-27 14:18:40 -0600

    Fixed root urls.py

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
